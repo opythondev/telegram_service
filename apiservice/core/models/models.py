de9b49da9b1fb0a019edcb18e6e647fc1daaacf0
Sergey <115350236+opythondev@users.noreply.github.com>
2023-06-22 17:25:27 +0300

    feat(api service): add celery
    add celery app with basic routes

--- apiservice/core/models/models.py
+++ apiservice/core/models/models.py
@@ -5,3 +5,8 @@ from typing import List
 class TaskInput(BaseModel):
     from_user: int
     channels: List[str]
+
+
+class TaskOut(BaseModel):
+    id: str
+    status: str
--- apiservice/core/routers/basic.py
+++ apiservice/core/routers/basic.py
@@ -1,9 +1,29 @@
-from fastapi import Request, APIRouter
-from core.models.models import TaskInput
+from fastapi import APIRouter
+from core.models.models import TaskInput, TaskOut
+from core.celery.app import add_channel_list_to_worker, dummy_task, celery_app
+from celery.result import AsyncResult
+
 
 router = APIRouter()
 
 
+def _to_task_out(r: AsyncResult) -> TaskOut:
+    return TaskOut(id=r.task_id, status=r.status)
+
+
+@router.get("/start")
+def start() -> TaskOut:
+    r = dummy_task.delay()
+    return _to_task_out(r)
+
+
+@router.get("/status")
+def status(task_id: str) -> TaskOut:
+    r = celery_app.AsyncResult(task_id)
+    return _to_task_out(r)
+
+
 @router.post("/fetch_channels")
-async def fetch_channels(request_data: TaskInput):
-    return request_data
+async def fetch_channels(request_data: TaskInput) -> TaskOut:
+    task = add_channel_list_to_worker.delay(request_data.channels)
+    return _to_task_out(task)
