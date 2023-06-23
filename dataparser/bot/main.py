cb59dcace65e97bea1b9491e617108658e47e8a1
sin0bee <hekpacoff@gmail.com>
2023-06-23 07:37:52 +0300

    bot structure create test queue

--- dataparser/bot/main.py
+++ dataparser/bot/main.py
@@ -1,15 +1,26 @@
+import logging
+
 from telethon import TelegramClient
 
 from .configs.env import API_ID, API_HASH
+from .task.task_manager import scheduler, manager, check_tasks
+from .task.test_tasks import create_fake_task
 
 
 async def __on_start_up():
-    print("start get tasks que")
+
+    logging.info("INIT on_start_up")
+
+    scheduler.add_job(func=create_fake_task, trigger="cron", minute="*/1")
+    scheduler.add_job(func=check_tasks, trigger="cron", minute="*/2", kwargs={"manager": manager})
+
+    scheduler.start()
 
 
 def start_bot():
     # TODO организовать проверку файла с сессией, если нет то сделать подключение и ответить на смс
-
+    logging.basicConfig(level=logging.INFO)
     client = TelegramClient('anon', API_ID, API_HASH)
     client.loop.create_task(__on_start_up())
+    logging.info("START BOT")
     client.loop.run_forever()
--- dataparser/bot/task/task.py
+++ dataparser/bot/task/task.py
@@ -1,4 +1,15 @@
+import dataclasses
+import datetime
+import uuid
+from typing import Any, Union
 
 
-class Task:
-    ...
+@dataclasses.dataclass
+class _Task:
+    """ task data unit """
+    task_id: uuid
+    trigger: str = "date"
+    run_time: Union[datetime, None] = None
+    kwargs: dict = None
+    foo: Any = None
+
