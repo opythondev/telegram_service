51572d6b5f54f78df2297a36460d95db41688e5f
Sergey <115350236+opythondev@users.noreply.github.com>
2023-07-28 14:53:32 +0300

    fix(ci): del flake_test.py

--- queueservice/flake_test.py
+++
@@ -1,3 +0,0 @@
-def some_func():
-    str = "jdkfkfkfkfkfkfkfkfkjdkfkfkfkfkfkfkfkfkjdkfkfkfkfkfkfkfkfkjdkfkfkfkfkfkfkfkfkjdkfkfkfkfkfkfkfkfk jdkfkfkfkfkfkfkfkfkjdkfkfkfkfkfkfkfkfkjdkfkfkfkfkfkfkfkfk"
-    return 1
