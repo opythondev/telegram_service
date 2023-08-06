42f656bf02f3041a9d416e93c6fe8227e1ecd985
sin0bee <hekpacoff@gmail.com>
2023-08-06 16:46:28 +0300

    Flake8 fix

--- dataparser/bot/clients.py
+++ dataparser/bot/clients.py
@@ -1,9 +1,12 @@
 from telethon import TelegramClient
 from bot.configs.env import CLIENTS
 
-client = TelegramClient('anon', CLIENTS['client'][0], CLIENTS['client'][1]).start()
-client2 = TelegramClient('client2', CLIENTS['client2'][0], CLIENTS['client2'][1]).start()
-client3 = TelegramClient('client3', CLIENTS['client3'][0], CLIENTS['client3'][1]).start()
+client = TelegramClient('anon',
+                        CLIENTS['client'][0], CLIENTS['client'][1]).start()
+client2 = TelegramClient('client2',
+                         CLIENTS['client2'][0], CLIENTS['client2'][1]).start()
+client3 = TelegramClient('client3',
+                         CLIENTS['client3'][0], CLIENTS['client3'][1]).start()
 
 
 
